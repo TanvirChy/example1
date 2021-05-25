d936aa706a2565eb5cbf2eee8e3d4ba98fc2f04a
TanvirChy <tanvirchy4242@gmail.com>
2021-05-26 02:19:38 +0600

    third commit

--- src/components/RowTable.jsx
+++ src/components/RowTable.jsx
@@ -2,34 +2,33 @@ import React, { useContext } from "react";
 import { RowContext } from "../contexts/RowContext";
 import BasicTable from './RowDetails';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
+// import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+// import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
-
-
+// const useStyles = makeStyles({
+//   table: {
+//     minWidth: 650,
+//   },
+// });
+   
 const RowTable = () => {
   const { rows } = useContext(RowContext);
   return rows.length ? (
-      <div className='Containar'>
+      <div >
           <ul>
               
-          <TableHead>
-           <TableRow>
+          <TableHead style={{backgroundColor:'purple'}}>
+           <TableRow >
             
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Action</TableCell>
+            <TableCell style={{padding:20}} align="right">Name</TableCell>
+            <TableCell className='Center' align="right">Email</TableCell>
+            <TableCell className='Right' align="right">Action</TableCell>
           </TableRow>
         </TableHead>
               { rows.map(row =>{
--- src/reducers/rowReducer.js
+++ src/reducers/rowReducer.js
@@ -1,18 +1,93 @@
 import { v4 as uuidv4 } from "uuid";
 
+export const rowReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ROW":
+      return [
+        ...state,
+        {
+          name: action.row.name,
+          email: action.row.email,
+          id: uuidv4(),
+        },
+      ];
 
-export const rowReducer = (state,action)=>{
-    switch(action.type){
-        case 'ADD_ROW':
-            return [...state,{
-                name:action.row.name,
-                email:action.row.email,
-                id:uuidv4()
-            }]
-        case 'REMOVE_ROW':
-            return state.filter(row => row.id !==action.id)
-        default:
-            return state
-    }
-}
+    case "REMOVE_ROW":
+      return state.filter((row) => row.id !== action.id);
+    default:
+      console.log("default");
+      return state;
+  }
+};
 
+// Try to remove insert duplicate  email
+
+// console.log(state)
+
+//             const tryN = state.map(dup=>{
+//                 dup.email.includes(action.row.email)
+//             })
+//             console.log(tryN)
+
+// console.log(state.rows.email)
+//             const val = state.includes(action.row.email)
+//             console.log(val)
+// if (state.indexOf(action.row.email) === 0) {
+//     return  [...state,{
+//            name:action.row.name,
+//             email:action.row.email,
+//              id:uuidv4()
+//          }];
+//   }
+
+// return [
+//   ...state,
+//   {
+//     name: action.row.name,
+//     email: action.row.email,
+//     id: uuidv4(),
+//   },
+// ];
+
+// const useremail = action.row.email
+//       console.log(useremail)
+//       state.map(dup=>{
+//         if(dup.email === useremail){
+//           console.log('no')
+//           return state
+//         }else{
+//           console.log('Yes')
+
+//           return [
+//             ...state,
+//             {
+//               name: action.row.name,
+//               email: action.row.email,
+//               id: uuidv4(),
+//             },
+//           ];
+//         }
+//       })
+
+    // return [
+    //   ...state,
+    //   {
+    //     name: action.row.name,
+    //     email: action.row.email,
+    //     id: uuidv4(),
+    //   },
+    // ];
+    //  state.map(dup=>{
+    //   if(dup.email ===action.row.email){
+    //     return [
+    //       ...state,
+    //       {
+    //         name: action.row.name,
+    //         email: action.row.email,
+    //         id: uuidv4(),
+    //       },
+    //     ];
+    //   }}else{
+    //     return state;
+
+    //    }
