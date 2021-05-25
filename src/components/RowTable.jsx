905de5feeb0d5747407e455bef9b9fb4581935f5
TanvirChy <tanvirchy4242@gmail.com>
2021-05-26 03:46:36 +0600

    forth commit

--- src/components/RowTable.jsx
+++ src/components/RowTable.jsx
@@ -1,47 +1,52 @@
 import React, { useContext } from "react";
 import { RowContext } from "../contexts/RowContext";
-import BasicTable from './RowDetails';
+import BasicTable from "./RowDetails";
 
-// import { makeStyles } from '@material-ui/core/styles';
-// import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-// import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-// import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
-// const useStyles = makeStyles({
-//   table: {
-//     minWidth: 650,
-//   },
-// });
-   
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
 const RowTable = () => {
   const { rows } = useContext(RowContext);
-  return rows.length ? (
-      <div >
-          <ul>
-              
-          <TableHead style={{backgroundColor:'purple'}}>
-           <TableRow >
-            
-            <TableCell style={{padding:20}} align="right">Name</TableCell>
-            <TableCell className='Center' align="right">Email</TableCell>
-            <TableCell className='Right' align="right">Action</TableCell>
+  const arr = rows;
+  const seen = new Set();
+  const sz = rows.length;
+  const filteredArr = arr.filter((el) => {
+    const duplicate = seen.has(el.email);
+    seen.add(el.email);
+    return !duplicate;
+  });
+  console.log("len=", rows.length);
+  console.log("rows :>> ", filteredArr.length);
+  return filteredArr.length ? (
+    <div className="Containar">
+      <ul>
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Action</TableCell>
           </TableRow>
         </TableHead>
-              { rows.map(row =>{
-                  return (<BasicTable row={row} key={row.id} />);
-              })}
-          </ul>
-      </div>
-  ):(
-      <div className='empty'> No row</div>
-  )
-}
-
-
-
+        {filteredArr.map((row) => {
+          return <BasicTable row={row} key={row.id} />;
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className="empty"> No row</div>
+  );
+};
 
 export default RowTable;
